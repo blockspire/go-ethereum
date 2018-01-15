988b5e0f1d5264f8dae6bd9df718157b6f0351e4
Pető Zoltán <bajgli@gmail.com>
2018-01-15 08:19:17 +0100

    WIP: observer chain scaffolding

--- les/observer/chain.go
+++ les/observer/chain.go
@@ -1,29 +1,63 @@
+// Copyright 2018 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
 package observer
 
 import (
+	"errors"
 	"time"
 
 	"github.com/ethereum/go-ethereum/ethdb"
+	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/trie"
 )
 
 // Chain ...
 type Chain struct {
+	config      *params.ChainConfig // Do we need any configuration?
+	chainDb     ethdb.Database
+	firstBlock  *Block
+	currentBlok *Block
 }
 
-// NewChain ...
-func NewChain(db ethdb.Database) *Chain {
-	return &Chain{}
+// NewChain returns a fully initialised Observer chain using information
+// available in the database
+func NewChain(db ethdb.Database) (*Chain, error) {
+
+	oc := &Chain{
+		chainDb: db,
+	}
+	oc.firstBlock, _ = oc.Block(0)
+	if oc.firstBlock == nil {
+		return nil, errors.New("First block not found in observer chain")
+	}
+	return oc, nil
 }
 
-// GetHead ...
-func (o *Chain) GetHead() *Block {
-	return &Block{}
+// Head returns the first block of Observer chain
+func (o *Chain) Head() *Block {
+	if o.firstBlock != nil {
+		return o.firstBlock
+	}
+	return nil
 }
 
-// GetBlock ...
-func (o *Chain) GetBlock(index uint64) *Block {
-	return &Block{}
+// Block returns a single block by its
+func (o *Chain) Block(index uint64) (*Block, error) {
+	return &Block{}, nil
 }
 
 // LockAndGetTrie lock trie mutex and get r/w access to the current observer trie
